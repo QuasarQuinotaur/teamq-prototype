60f27278ce4a8dfac6d7b90d13b40160e1580a6b
ben-turtles <179182450+ben-turtles@users.noreply.github.com>
2026-04-01 14:17:58 -0400

    Create view selector button

--- apps/frontend/src/components/MinorTopbar.tsx
+++ apps/frontend/src/components/MinorTopbar.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
     NavigationMenu,
     NavigationMenuItem,
@@ -12,13 +14,17 @@ import {
 import {
     MagnifyingGlassIcon,
     FunnelSimpleIcon,
-    ArrowsDownUpIcon
+    ArrowsDownUpIcon,
+    ListBulletsIcon,
+    GridFourIcon,
 } from "@phosphor-icons/react";
 import {
-    Popover, PopoverContent,
+    Popover,
+    PopoverContent,
     PopoverTrigger
 } from "@/components/ui/popover.tsx";
 import { Button } from "@/components/ui/button.tsx";
+import {Item} from "@/components/ui/item.tsx";
 
 function FilterButton() {
     return (
@@ -54,50 +60,74 @@ function SortButton() {
     )
 }
 
+const viewTypes = {
+    List: {
+        icon: <ListBulletsIcon/>
+    },
+    Grid: {
+        icon: <GridFourIcon/>
+    }
+}
+type ViewType = keyof typeof viewTypes
 function ViewSelectorButton() {
+    // Would pass in props.setViewType() to toggle on page
+    const [selectedViewType, setSelectedViewType] = useState<ViewType>("List")
+
     return (
-        <Button variant={"outline"}>
-            View Selector
-        </Button>
+        <Item variant={"outline"} className={"gap-0.5 py-0 px-0"}>
+            {Object.entries(viewTypes).map(([viewType, iconType]) => (
+                <Button
+                    id={viewType}
+                    variant={viewType == selectedViewType ? "default" : "ghost"}
+                    onClick={() => {
+                        if (viewType != selectedViewType) {
+                            setSelectedViewType(viewType as ViewType)
+                        }
+                    }}
+                >
+                    {iconType.icon}
+                </Button>
+            ))}
+        </Item>
     )
 }
 
 export default function MinorTopbar() {
     return (
-            <NavigationMenu className={"max-w-full"}>
-                {/*Left Bar*/}
-                <NavigationMenuList className={"flex w-full"}>
-                    <Separator
-                        orientation="vertical"
-                        className="mr-2 data-[orientation=vertical]"/>
-                    <NavigationMenuItem>
-                        <InputGroup>
-                            <InputGroupInput
-                                id={"minor-topbar-search"}
-                                placeholder={"Search..."}
-                                // value={name}
-                                // onChange={(e) =>
-                                //     setName(e.target.value)}
-                            />
-                            <InputGroupAddon align={"inline-end"}>
-                                <MagnifyingGlassIcon/>
-                            </InputGroupAddon>
-                        </InputGroup>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-                <div className="flex-1"/>
-                {/*Right Bar*/}
-                <NavigationMenuList className={"gap-1"}>
-                    <NavigationMenuItem>
-                        <FilterButton/>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <SortButton/>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <ViewSelectorButton/>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
+        <NavigationMenu className={"max-w-full"}>
+            {/*Left Bar*/}
+            <NavigationMenuList className={"flex w-full"}>
+                <Separator
+                    orientation="vertical"
+                    className="mr-2 data-[orientation=vertical]"/>
+                <NavigationMenuItem>
+                    <InputGroup>
+                        <InputGroupInput
+                            id={"minor-topbar-search"}
+                            placeholder={"Search..."}
+                            // value={name}
+                            // onChange={(e) =>
+                            //     setName(e.target.value)}
+                        />
+                        <InputGroupAddon align={"inline-end"}>
+                            <MagnifyingGlassIcon/>
+                        </InputGroupAddon>
+                    </InputGroup>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+            <div className="flex-1"/>
+            {/*Right Bar*/}
+            <NavigationMenuList className={"gap-1"}>
+                <NavigationMenuItem>
+                    <FilterButton/>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <SortButton/>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <ViewSelectorButton/>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
     )
 }
