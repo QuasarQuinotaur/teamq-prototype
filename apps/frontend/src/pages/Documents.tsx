b06ad467772e4ea4e2a142d5d7ae86f578966828
nj-anderson <norahjanderson@gmail.com>
2026-04-02 12:48:37 -0400

    fixed Documents page to render subpages

--- apps/frontend/src/pages/Documents.tsx
+++ apps/frontend/src/pages/Documents.tsx
@@ -25,6 +25,7 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 import MinorTopbar from "@/components/MinorTopbar.tsx";
+import { Outlet } from "react-router-dom";
 
 export default function Documents() {
   return (
@@ -50,13 +51,8 @@ export default function Documents() {
             {/*</Breadcrumb>*/}
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-          </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+          <Outlet />
         </div>
       </SidebarInset>
     </SidebarProvider>
