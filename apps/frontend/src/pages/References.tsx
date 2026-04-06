208c9b9d5a4dbe961154a8d7608db341389cef96
ben-turtles <benturtlesdev@gmail.com>
2026-04-06 15:17:25 -0400

    merge: Reference + Tools links array

--- apps/frontend/src/pages/References.tsx
+++ apps/frontend/src/pages/References.tsx
@@ -1,65 +1,52 @@
 import MinorTopbar from "@/components/MinorTopbar.tsx";
-import CardImage from "../components/cardImage.tsx";
 import PaginationControl from "../components/paginationControl"
+import {
+    CardGrid,
+    type CardEntry
+} from "@/components/CardGrid.tsx";
 
 
 function References() {
+    // TODO get from backend
+    const entries: CardEntry[] = [
+        {
+            title: "ISOnet Website",
+            link: "https://github.com"
+        },
+        {
+            title: "Forms Knowledge Base",
+            link: "https://github.com"
+        },
+        {
+            title: "Coastal Guidelines",
+            link: "https://github.com"
+        },
+        {
+            title: "CPP Rater",
+            link: "https://github.com"
+        },
+        {
+            title: "Experience & Schedule Rating Plans",
+            link: "https://github.com"
+        },
+        {
+            title: "States on Hold",
+            link: "https://github.com"
+        },
+        {
+            title: "PMS URG",
+            link: "https://github.com"
+        },
+    ]
 
 
     return (
         <>
             <MinorTopbar/>
-            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                <CardImage
-                    title="ISOnet Website"
-                    description=""
-                    badge="Reference"
-                    action="View"
-                    link="https://github.com"
-                />
-                <CardImage
-                    title="Forms Knowledge Base"
-                    description=""
-                    badge="Reference"
-                    action="View"
-                    link="https://github.com"
-                />
-                <CardImage
-                    title="Coastal Guidelines"
-                    description=""
-                    badge="Reference"
-                    action="View"
-                    link="https://github.com"
-                />
-                <CardImage
-                    title="CPP Rater"
-                    description=""
-                    badge="Reference"
-                    action="View"
-                    link="https://github.com"
-                />
-                <CardImage
-                    title="Experience & Schedule Rating Plans"
-                    description=""
-                    badge="Reference"
-                    action="View"
-                    link="https://github.com"
-                />
-                <CardImage
-                    title="States on Hold"
-                    description=""
-                    badge="Reference"
-                    action="View"
-                    link="https://github.com"
-                />
-                <CardImage
-                    title="PMS URG"
-                    description=""
-                    badge="Reference"
-                    action="View"
-                    link="https://github.com"
-                />
-            </div>
+            <CardGrid
+                entries={entries}
+                defaultBadge={"Reference"}
+            />
             <div>
                 <PaginationControl docNum={7}/>
             </div>
