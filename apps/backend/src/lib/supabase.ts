bc56dfcfbda594b1b2e59e65c62ab195b719c9b0
Benjamin Santana <ben@Benjamins-MacBook-Pro-822.local>
2026-04-08 18:51:28 -0400

    debugging env variables

--- apps/backend/src/lib/supabase.ts
+++ apps/backend/src/lib/supabase.ts
@@ -1,5 +1,8 @@
 import "dotenv/config"
 
+console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+console.log("SUPABASE_SECRET_KEY:", process.env.SUPABASE_SECRET_KEY);
+
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
