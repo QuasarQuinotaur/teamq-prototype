683be82902123ecc0a2b0b5fe3cbb39da40a5b72
TheronBoozer <boozerwt@gmail.com>
2026-04-15 00:36:18 -0400

    default and more

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import {Outlet} from 'react-router-dom'
 import ErrorWrapper from './pages/Error'
+import { useEffect } from "react";
 
 
 export default function App() {
+
+  useEffect(() => {
+    document.documentElement.setAttribute("class", "theme-default");
+  });
+
   return (
     <ErrorWrapper>
     <div className="app-shell">
